be1022c30a5ceb5be56ddc27d23febe89da303ab
eBuildy <ebuildy@gmail.com>
2015-03-01 21:25:34 +0100

    [ios] Move to new Photo API

--- src/ios/GalleryAPI.m
+++ src/ios/GalleryAPI.m
@@ -10,92 +10,97 @@
 
 - (void) getAlbums:(CDVInvokedUrlCommand*)command
 {
-    __block ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
     __block NSMutableArray *albums = [[NSMutableArray alloc] init];
+    NSArray *collectionTypes = @[
+                                 @{@"title" : @"smart", @"type" : [NSNumber numberWithInteger: PHAssetCollectionTypeSmartAlbum]},
+                                 @{@"title" : @"album", @"type" : [NSNumber numberWithInteger:PHAssetCollectionTypeAlbum]},
+                                 @{@"title" : @"moment", @"type" : [NSNumber numberWithInteger:PHAssetCollectionTypeMoment]}
+    ];
     
-    [library enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop)
-     {
-         if (group == nil)
-         {
-             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:albums];
-
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-         }
-         else
-         {
-             [albums addObject:@{
-                                 @"id" : [group valueForProperty:ALAssetsGroupPropertyPersistentID],
-                                 @"title" : [group valueForProperty:ALAssetsGroupPropertyName],
-                                 @"assets" : [NSString stringWithFormat:@"%ld", group.numberOfAssets]
-                                 }];
-         }
-         
-     }
-     failureBlock: ^(NSError *error)
+    for (NSDictionary *collectionType in collectionTypes)
     {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+        [[PHAssetCollection fetchAssetCollectionsWithType:[[collectionType objectForKey:@"type"] integerValue] subtype:PHAssetCollectionSubtypeAny options:nil] enumerateObjectsUsingBlock:^(PHAssetCollection *collection, NSUInteger idx, BOOL *stop)
+         {
+             if (collection != nil && collection.localizedTitle != nil && collection.localIdentifier != nil)
+             {
+                 [albums addObject:@{
+                                     @"id" : collection.localIdentifier,
+                                     @"title" : collection.localizedTitle,
+                                     @"type" : [collectionType objectForKey:@"title"],
+                                     @"assets" : [NSString stringWithFormat:@"%ld", (long) collection.estimatedAssetCount]
+                                     }];
+             }
+         }];
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:albums];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void) getMedia:(CDVInvokedUrlCommand*)command
 {
     NSString *album = [command argumentAtIndex:0];
-    __block ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
     __block NSMutableArray *assets = [[NSMutableArray alloc] init];
+    __block PHImageRequestOptions * imageRequestOptions = [[PHImageRequestOptions alloc] init];
     
-    [library enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop)
-     {
-         if (group == nil)
+    imageRequestOptions.synchronous = YES;
+    
+    NSArray *collectionTypes = @[
+                                 @{@"title" : @"smart", @"type" : [NSNumber numberWithInteger: PHAssetCollectionTypeSmartAlbum]},
+                                 @{@"title" : @"album", @"type" : [NSNumber numberWithInteger:PHAssetCollectionTypeAlbum]},
+                                 @{@"title" : @"moment", @"type" : [NSNumber numberWithInteger:PHAssetCollectionTypeMoment]}
+                                 ];
+    
+    for (NSDictionary *collectionType in collectionTypes)
+    {
+        [[PHAssetCollection fetchAssetCollectionsWithType:[[collectionType objectForKey:@"type"] integerValue] subtype:PHAssetCollectionSubtypeAny options:nil] enumerateObjectsUsingBlock:^(PHAssetCollection *collection, NSUInteger idx, BOOL *stop)
          {
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:assets];
-
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-         }
-         else
-         {
-             if ([album isEqualToString:[group valueForProperty:ALAssetsGroupPropertyName]])
+             if (collection != nil && collection.localizedTitle != nil && [album isEqualToString:collection.localizedTitle])
              {
-                 [group enumerateAssetsWithOptions:NSEnumerationReverse usingBlock:
-                  ^(ALAsset *result, NSUInteger index, BOOL *stop)
-                  {
-                      if(result == nil)
+                 [[PHAsset fetchAssetsInAssetCollection:collection options:nil] enumerateObjectsUsingBlock:^(PHAsset *obj, NSUInteger idx, BOOL *stop)
+                 {
+                     [[PHImageManager defaultManager]
+                      requestImageDataForAsset:obj
+                      options:imageRequestOptions
+                      resultHandler:^(NSData *imageData, NSString *dataUTI,
+                                      UIImageOrientation orientation,
+                                      NSDictionary *info)
                       {
-                            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:assets];
-
-                            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                      }
-                      else
-                      {
-                          ALAssetRepresentation *representation = result.defaultRepresentation;
-                          CGSize dimensions = representation.dimensions;
-                        
+                          NSString *filename = @"";
+                          NSString *path = @"";
+                          
+                          if ([info objectForKey:@"PHImageFileUTIKey"])
+                          {
+                              filename = [info objectForKey:@"PHImageFileUTIKey"];
+                          }
+                          
+                          if ([info objectForKey:@"PHImageFileURLKey"])
+                          {
+                              path = [[info objectForKey:@"PHImageFileURLKey"] absoluteString];
+                          }
+                          
                           [assets addObject:@{
-                                              @"id" : [[result valueForProperty:ALAssetPropertyAssetURL] absoluteString],
-                                              @"title" : representation.filename,
+                                              @"id" : obj.localIdentifier,
+                                              @"title" : filename,
                                               @"orientation" : @"up",
                                               @"lat" : @4,
                                               @"lng" : @5,
-                                              @"width" : [NSNumber numberWithFloat:dimensions.width],
-                                              @"height" : [NSNumber numberWithFloat:dimensions.height],
-                                              @"size" : [NSNumber numberWithLongLong:representation.size],
-                                              @"data" : representation.url.absoluteString,
-                                              @"thumbnail" : representation.url.absoluteString
-                          }];
-                      }
-                  }
-                  ];
+                                              @"width" : [NSNumber numberWithFloat:obj.pixelWidth],
+                                              @"height" : [NSNumber numberWithFloat:obj.pixelHeight],
+                                              @"size" : @0,
+                                              @"data" : path,
+                                              @"thumbnail" : @""
+                                              }];
+                      }];
+                 }];
              }
-             
-         }
-     }
-     failureBlock: ^(NSError *error)
-    {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+         }];
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:assets];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 + (NSString*)cordovaVersion
